3b0869778cba5ca50527062fc6cf75be755c0a50
Olga Minguett <olgaminguett@gmail.com>
2017-04-20 21:15:48 +0100

    OnlineBank3 Client
    GUI interface almost was done. 
    POST and GET for withdrawing, lodgement, and transfer (To do)
    Login, register, new account connected. 
    date of birth doesn't crash.
    login is done with no account pre set.
    DB updated

---
+++ OnlineBank3Client/target/generated-sources/annotations/com/mycompany/models/Customers_.java
@@ -0,0 +1,27 @@
+package com.mycompany.models;
+
+import com.mycompany.models.Accounts;
+import com.mycompany.models.Transactions;
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.CollectionAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-19T14:29:20")
+@StaticMetamodel(Customers.class)
+public class Customers_ { 
+
+    public static volatile SingularAttribute<Customers, String> firstname;
+    public static volatile SingularAttribute<Customers, String> password;
+    public static volatile CollectionAttribute<Customers, Transactions> transactionsCollection1;
+    public static volatile SingularAttribute<Customers, String> address;
+    public static volatile CollectionAttribute<Customers, Accounts> accountsCollection;
+    public static volatile SingularAttribute<Customers, Integer> customerId;
+    public static volatile SingularAttribute<Customers, Date> dateOfBirth;
+    public static volatile CollectionAttribute<Customers, Transactions> transactionsCollection;
+    public static volatile SingularAttribute<Customers, String> user;
+    public static volatile SingularAttribute<Customers, String> email;
+    public static volatile SingularAttribute<Customers, String> lastname;
+
+}
